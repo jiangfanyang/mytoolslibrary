952fc815b99b768838a2b8b2da6eb09529ccb9fd
ltf <123456>
2017-02-23 16:38:45 +0800

    工具库

--- app/src/main/java/com/ltf/tools/mytoolslibrary/MainActivity.java
+++ app/src/main/java/com/ltf/tools/mytoolslibrary/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PicassoUtil.getInstantiation().onWidgetImage(this,"", (ImageView) findViewById(R.id.imgs));
+        PicassoUtil.getInstantiation().onWidgetImage(this,"https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1487839107&di=934a0f03c91b97da2fbd2774e2a1590c&src=http://pic12.nipic.com/20110213/580124_212334691148_2.jpg", (ImageView) findViewById(R.id.imgs));
     }
 }
